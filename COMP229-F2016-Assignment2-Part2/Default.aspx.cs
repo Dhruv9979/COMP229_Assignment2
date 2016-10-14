8d927c7bdca8dc1fdbbc1d6f4b9d39ee4ceba1e5
Dhruv Patel <Dhruv Patel@DESKTOP-2O5G41M>
2016-10-14 04:06:59 -0400

    Add project files.

---
+++ COMP229-F2016-Assignment2-Part2/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//using statements that are required to connect to EF_DB
+using System.Web.ModelBinding;
+using COMP229_F2016_Assignment2_Part2.Models;
+
+namespace COMP229_F2016_Assignment2_Part2
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if loading the page for the first time 
+            //populate the cricket grid
+
+            if (!IsPostBack)
+            {
+                this.getCricket();
+            }
+        }
+        
+    }
+}
---
+++ COMP229-F2016-Assignment2-Part2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="COMP229_F2016_Assignment2_Part2.Global" Language="C#" %>
